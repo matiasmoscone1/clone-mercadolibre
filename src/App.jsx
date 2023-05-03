52961b27b985d7f57f771bdcbf35602bdbf420cc
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-03 14:09:02 -0300

    Empezando proyecto de clone-mercadolibre

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+
+
+function App() {
+
+
+
+
+
+  const Navbar = () => {
+    return(<div>
+      <div> Logo </div>       
+      
+      <div> <input /> Input Busqueda</div>
+    
+      <div> Promocion starDisney </div>
+
+      <div> Envios </div>
+
+      <div> Categoria/varios</div>
+
+      <div> Panel de usuario </div>
+
+
+    </div>)
+  }
+
+
+
+  return (
+   <>
+   
+   <Navbar />
+
+   <h2>Hola</h2>
+   
+   </>
+  )
+}
+
+export default App
