601829dfbff28140d0e9735dd8a7654a12de8cdd
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-03 14:21:39 -0300

    Creando esqueleto de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@
 function App() {
 
 
-
-
-
   const Navbar = () => {
     return(<div>
       <div> Logo </div>       
@@ -21,19 +18,112 @@ function App() {
 
       <div> Panel de usuario </div>
 
-
     </div>)
   }
 
 
+  const Carrousel = () => {
+    return(<div>
+      Carrousel
+    </div>)
+  }
+
+  const FormasPago = () => {
+    return(<div>
+      Formas de pago
+    </div>)
+  }
+
+
+  const Ofertas = () => {
+    return(<div>
+      Ofertas
+    </div>)
+  }
+
+  const Subscripcion = () => {
+    return(<div>
+      Subscripcion
+    </div>)
+  }
+
+  const BeneficioMercadoPuntos = () => {
+    return(<div>
+      Beneficio Mercado Puntos
+    </div>)
+  }
+
+  const Publicidad = () => {
+    return(<div>
+      Publicidad
+    </div>)
+  }
+
+  const Descubri = () => {
+    return(<div>
+      Descubri
+    </div>)
+  }
+
+  const Tiendas = () => {
+    return(<div>
+      Tiendas
+    </div>)
+  }
+
+  const TePuedeInteresar = () => {
+    return(<div>
+      Te puede interesar
+    </div>)
+  }
+
+  const Colecciones = () => {
+    return(<div>
+      Colecciones
+    </div>)
+  }
+
+  const CategoriasPopulares = () => {
+    return(<div>
+      Categorias Populares
+    </div>)
+  }
+
+  const Footer = () => {
+    return(<div>
+      Footer
+    </div>)
+  }
 
   return (
    <>
    
-   <Navbar />
+    <Navbar />
 
-   <h2>Hola</h2>
-   
+    <Carrousel /> 
+
+    <FormasPago />
+
+    <Ofertas />
+
+    <Subscripcion />
+
+    <BeneficioMercadoPuntos />
+
+    <Publicidad />
+
+    <Descubri />
+
+    <Tiendas />
+
+    <TePuedeInteresar />
+
+    <Colecciones/>
+
+    <CategoriasPopulares />
+
+    <Footer />
+
    </>
   )
 }
